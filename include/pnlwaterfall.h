38fd8c9a434f43d92227c1dbce42cf93cc94dcd1
Christian Müller <christian@chrmue.de>
2021-05-12 22:31:57 +0200

    added to last commit

--- include/pnlwaterfall.h
+++ include/pnlwaterfall.h
@@ -2,6 +2,6 @@
 #define PNLWATERFALL_H
 #include "cmenue.h"
 int initFkeysWaterPan(cPanel* pan, tCoord lf);
-int createWaterPan(cPanel* pan, tCoord lf);
+int initWaterPan(cPanel* pan, tCoord lf);
 int initTimePan(cPanel* pan, tCoord lf);
 #endif // PNLWATERFALL_H
