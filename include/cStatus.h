7175505900411f9da2d041d1c3857821be26b778
Christian Müller <chrmue44@gmail.com>
2023-12-23 22:10:36 +0100

    cStatus.h

--- include/cStatus.h
+++ include/cStatus.h
@@ -1,36 +1,68 @@
+/*
+*  Display mode | LED 1 |  LED2
+*  -------------+-------+---------------------------------------------
+*  Batt         |  1x   |  100% = 4x, 75% = 3x, 50% = 2x, 25% = 1x
+*  SD-Card      |  2x   |  100% = 4x, 75% = 3x, 50% = 2x, 25% = 1x
+*  Rec          |  3x   |  active = 1x, inactive = 0x, recording = ON
+*/
+
 #include "stdint.h"
+#include "cTimer.h"
 
-struct stStatusItem 
-{
-  bool on;
-  int ms;
-}; 
+#define LED_ON_TIME  70
+#define LED_OFF_TIME 300
+#define REPEAT_TIME  3000
 
-enum enStatLed1
+enum enStatLed
 {
-  IDLE = 0,
-  ON,
+  LED_OFF = 0,
+  LED_ON,
   ON_100,
   ON_75,
   ON_50,
   ON_25  
 };
 
+enum enLedMode
+{
+  DISP_IDLE = 0,
+  DISP_BATT = 1,
+  DISP_SD = 2,
+  DISP_REC = 3,
+};
+
+enum enShowState
+{
+  INIT = 0,
+  WAIT = 1,
+  BLINK_MODE = 2,
+  BLINK_STATE = 3
+};
+
 class cStatus
 {
   private:
-   enStatLed1 _statusLed1;
-   int _subStateLed1;
+   enStatLed _statusLed2 = enStatLed::LED_OFF;
+   enLedMode _ledMode = enLedMode::DISP_IDLE;
    uint32_t _lastT = 0;
-   uint32_t _nextEndTime;
-   const stStatusItem* _psubStateLed1;
-   const stStatusItem* _psubStateStart1;
+   uint32_t _maxBlinkCnt1 = 0;   
+   uint32_t _maxBlinkCnt2 = 0;   
+   uint32_t _blinkCnt1 = 0;   
+   uint32_t _blinkCnt2 = 0;
+   enShowState _state = enShowState::INIT; 
+   bool _led1On = false;
+   bool _oldLed1On = true;
+   bool _led2On = false;
+   bool _oldLed2On = true;
+   cTimer _timer;
 
   public:
    cStatus() {}
    void show();
-   void setStateLed1(enStatLed1 state);
+   void nextState();
 
   private:
-   void setNewSubState();
+   void setStateLed2(enStatLed state);
+   void setLedMode(enLedMode mode);
+   void updateStatus();
 };
